154c42adc7c7e0d892c69aa8a415f0878765b76d
ar8372 <ar837232342@gmail.com>
2022-03-17 09:07:25 +0000

    create_folds.py add

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,16 +1,39 @@
 import pandas as pd
 from sklearn import model_selection
+import os 
+import sys 
+import pickle
+
+"""
+import os 
+import sys 
+import pickle
+with open(os.path.join(sys.path[0], "ref.txt"), "r") as x:
+    for i in x:
+        comp_name = i
+x.close()
+with open(f"../models_{comp_name}/locker.pkl", "rb") as f:
+    a = pickle.load(f)
+"""
 
 if __name__ == "__main__":
-    df = pd.read_csv("input/train_cat2.csv")
+    with open(os.path.join(sys.path[0], "ref.txt"), "r") as x:
+        for i in x:
+            comp_name = i
+    x.close()
+    with open(f"../models_{comp_name}/locker.pkl", "rb") as f:
+        a = pickle.load(f)
+
+    df = pd.read_csv(f"../input_{comp_name}/train.csv")
     df["fold"] = -1
 
     df = df.sample(frac=1).reset_index(drop=True)
     kf = model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=23)
 
-    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
+    target_name = a['target_name']
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df[target_name].values)):
         print(len(train_idx), len(val_idx))
 
         df.loc[val_idx, "fold"] = fold
 
-    df.to_csv("input/train_folds.csv", index=False)
+    df.to_csv(f"../input_{comp_name}/train_folds.csv", index=False)
--- src/keys.py
+++ src/keys.py
@@ -10,25 +10,26 @@ class KeyMaker:
         random_state=21,
         target_name="Survived",
         id_name="Passenger_ID",
-        comp_type="binary",
+        comp_index=0
     ):
         #
         with open(os.path.join(sys.path[0], "ref.txt"), "r") as x:
             for i in x:
                 comp_name = i
         x.close()
+        self.comp_list = ["regression", "2class","multi_class", "multi_label"]
         self._comp_name = comp_name
         self.random_state = random_state
         self.target_name = target_name
         self.id_name = id_name
+        self.comp_type = self.comp_list[comp_index]
         self.locker = defaultdict()
-        self.locker["comp_name"] = self._comp_name
-        self.locker["random_state"] = self.random_state
-        self.locker["target_name"] = self.target_name
-        self.locker["id_name"] = self.id_name
         self.update()  # dumps files as pickel
 
-    def __call__(self, random_state="--|--", target_name="--|--", id_name="--|--"):
+    def help(self):
+        print("comp_index:=> ","0: regression", "1: 2class","2: multi_class", "3: multi_label")
+
+    def __call__(self, random_state="--|--", target_name="--|--", id_name="--|--", comp_type="--|--"):
         with open(os.path.join(sys.path[0], "ref.txt"), "r") as x:
             for i in x:
                 comp_name = i
@@ -38,6 +39,8 @@ class KeyMaker:
         self.random_state = a["random_state"]
         self.target_name = a["target_name"]
         self.id_name = a["id_name"]
+        self.comp_type = a["comp_type"]
+
         if random_state != "--|--":
             # updated
             self.random_state = random_state
@@ -47,6 +50,9 @@ class KeyMaker:
         if id_name != "--|--":
             # updated
             self.id_name = id_name
+        if comp_type != "--|--":
+            self.comp_type = comp_type
+
         self.update()  # dump files to pickel
 
     def update(self):
@@ -56,6 +62,7 @@ class KeyMaker:
         self.locker["random_state"] = self.random_state
         self.locker["target_name"] = self.target_name
         self.locker["id_name"] = self.id_name
+        self.locker["comp_type"] = self.comp_type
         with open(f"../models_{a['comp_name']}/locker.pkl", "wb") as f:
             pickle.dump(self.locker, f)
 
